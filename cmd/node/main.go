42c5f0371c190e8d1d44bcffb427a7829fb10a96
kev-techi <kevin@techi.be>
2024-03-13 14:46:52 +0100

    make -env optional again

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -45,7 +45,7 @@ import (
 func main() {
 	log.Default().Println("launching indexer...")
 
-	env := flag.String("env", ".env", "path to .env file")
+	env := flag.String("env", "", "path to .env file")
 
 	certpath := flag.String("certpath", "./certs", "cert folder path")
 

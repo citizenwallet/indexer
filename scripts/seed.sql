dc99979a01755f7159cdfdb255b649d9a4bc3959
kev-techi <kevin@techi.be>
2024-03-27 13:31:01 +0100

    update seed script

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -19,7 +19,7 @@ CREATE INDEX IF NOT EXISTS idx_events_84532_address_signature ON t_events_84532(
 CREATE INDEX IF NOT EXISTS idx_events_84532_address_signature_state ON t_events_84532(contract, standard, state);
 
 INSERT INTO t_events_84532(contract, state, created_at, updated_at, start_block, last_block, standard, name, symbol, decimals)
-    VALUES ('0x9b1a0D2951b11Ac26A6cBbd5aEf2c4cb014b3B6e', 'indexed', '2024-03-08 12:02:25.862592+01:00', '2024-03-08 12:02:25.862592+01:00', '32945384', '32945384', 'ERC20', 'ETHGlobal London Token', 'ETHLDN', 6);
+    VALUES ('<token-contract>', 'indexed', '2024-03-08 12:02:25.862592+01:00', '2024-03-08 12:02:25.862592+01:00', '32945384', '32945384', 'ERC20', '<token-name>', '<token-symbol>', 6);
 
 CREATE TABLE IF NOT EXISTS t_sponsors_84532(
     contract text NOT NULL PRIMARY KEY,
@@ -29,5 +29,5 @@ CREATE TABLE IF NOT EXISTS t_sponsors_84532(
 );
 
 INSERT INTO t_sponsors_84532(contract, pk, created_at, updated_at)
-    VALUES ('0x389182aCCeE26D953d5188BF4b92c49339DcC9FC', 'HktJpk6mLNkUwEdHwe8CjXDytxy9yuJCbWwB_Vmh8ei3DD_HdmXl44lfiGRtxwhUIMnU8rEzxHeGeWsWCyG0_4IkcoGlY7H5BSJiKpTkG2c=', '2024-03-08 12:02:25.862592+01:00', '2024-03-08 12:02:25.862592+01:00');
+    VALUES ('<paymaster-contract>', '<encrypted-key>', '2024-03-08 12:02:25.862592+01:00', '2024-03-08 12:02:25.862592+01:00');
 
